93fbe25ac144c6e3e9f2b2338cd8b5327d916f6b
FIRMAN_MAULANA_A <abdurrasyid1106@gmail.com>
2023-12-17 18:06:16 +0700

    update data python

---
+++ LATIHAN PYTHON/19.py
@@ -0,0 +1,16 @@
+#width and multiline
+
+nama = "Firman Maulana Akbar"
+umur = 24
+tinggi = 165.0
+nosepatu = 42
+
+#string
+data_string = f"nama = {nama}, umur = {umur}, tinggi = {tinggi}, no sepatu = {nosepatu}"
+print(5*"="+"DATA STRING"+5*"=")
+print(data_string)
+
+#string multiline
+data_string = f"nama = {nama},\numur = {umur},\ntinggi = {tinggi},\nno sepatu = {nosepatu}"
+print(5*"="+"DATA STRING"+5*"=")
+print(data_string)
---
+++ LATIHAN PYTHON/test.py
@@ -0,0 +1,27 @@
+# variabel tempat menyimpan data
+
+a = 20
+print("Nilai a =", a)
+# penamaan
+nilai_y = 500 #ga boleh pakai spasi dan gaboleh awalan angka
+
+#pemanggilan
+print(a)
+#bisa ganti nilai a
+a = 100
+print(a)
+b = a + 100
+print(b)
+c = "ini huruf C"
+print(c)
+
+# type data dasar eps 5
+# angka satuan (integer)
+data_integer = 1
+#float angka dengan koma)
+data_float = 1.5
+print(data_float)
+
+# data string adalah kumpulan karakter yang paek tanda kutip
+#data boolean bernilai true or false
+
